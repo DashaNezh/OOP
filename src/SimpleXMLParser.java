806dda32e8c71c0de8366fbd1b150e70cbbda8c2
DashaNezh <melkomukova.dara@mail.ru>
2024-10-03 21:55:18 +0500

    Доделал лабу

--- src/SimpleXMLParser.java
+++ src/SimpleXMLParser.java
@@ -1,79 +1,102 @@
-import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
+import javax.xml.parsers.*;
+import org.w3c.dom.*;
 
-// Класс Library, который содержит список книг
 class Library {
-    List<Book> books = new ArrayList<>();
+    private List<Book> books = new ArrayList<>();
+
+    public void addBook(Book book) {
+        books.add(book);
+    }
 
     @Override
     public String toString() {
-        StringBuilder sb = new StringBuilder();
-        for (Book book : books) {
-            sb.append(book.toString()).append("\n");
-        }
-        return sb.toString();
+        return books.stream()
+                .map(Book::toString)
+                .collect(Collectors.joining("\n\n", "Library:\n", ""));
     }
 }
 
-// Класс Book, который хранит информацию о книге
 class Book {
-    String id;
-    String title;
-    String author;
-    int year;
-    String genre;
-    double price;
-    String currency;
-    List<Review> reviews = new ArrayList<>();
-    String language;
-    Publisher publisher;
-    String isbn;
-    String format;
-    List<String> awards = new ArrayList<>();
+    private String id, title, author, genre, language, isbn, format, currency;
+    private int year;
+    private double price;
+    private Publisher publisher;
+    private List<Review> reviews = new ArrayList<>();
+    private List<String> awards = new ArrayList<>();
+
+    public Book(String id, String title, String author, int year, String genre, double price, String currency,
+                String language, Publisher publisher, String isbn, String format, List<Review> reviews, List<String> awards) {
+        this.id = id;
+        this.title = title;
+        this.author = author;
+        this.year = year;
+        this.genre = genre;
+        this.price = price;
+        this.currency = currency;
+        this.language = language;
+        this.publisher = publisher;
+        this.isbn = isbn;
+        this.format = format;
+        this.reviews = reviews;
+        this.awards = awards;
+    }
 
     @Override
     public String toString() {
-        return "Book ID: " + id + "\nTitle: " + title + "\nAuthor: " + author +
-                "\nYear: " + year + "\nGenre: " + genre + "\nPrice: " + price + " " + currency +
-                "\nLanguage: " + language + "\nPublisher: " + (publisher != null ? publisher.toString() : "N/A") +
-                "\nReviews: " + reviews + "\nAwards: " + awards;
+        return String.format("ID: %s\nTitle: %s\nAuthor: %s\nYear: %d\nGenre: %s\nPrice: %.2f %s\nLanguage: %s\nPublisher: %s\nISBN: %s\nFormat: %s\nReviews:\n%s\nAwards: %s",
+                id, title, author, year, genre, price, currency, language,
+                publisher != null ? publisher : "N/A", isbn, format,
+                reviews.isEmpty() ? "No reviews" : reviews.stream().map(Review::toString).collect(Collectors.joining("\n")),
+                awards.isEmpty() ? "No awards" : String.join(", ", awards));
     }
 }
 
-// Класс Review для хранения отзывов о книге
 class Review {
-    String user;
-    int rating;
-    String comment;
+    private String user, comment;
+    private int rating;
+
+    public Review(String user, int rating, String comment) {
+        this.user = user;
+        this.rating = rating;
+        this.comment = comment;
+    }
 
     @Override
     public String toString() {
-        return "User: " + user + ", Rating: " + rating + ", Comment: " + comment;
+        return String.format("User: %s, Rating: %d, Comment: %s", user, rating, comment);
     }
 }
 
-// Класс Publisher, который содержит информацию об издателе
 class Publisher {
-    String name;
-    Address address;
+    private String name;
+    private Address address;
+
+    public Publisher(String name, Address address) {
+        this.name = name;
+        this.address = address;
+    }
 
     @Override
     public String toString() {
-        return "Publisher: " + name + ", Address: " + address;
+        return String.format("Publisher: %s, Address: %s", name, address);
     }
 }
 
-// Класс Address для хранения адреса издателя
 class Address {
-    String city;
-    String country;
+    private String city, country;
+
+    public Address(String city, String country) {
+        this.city = city;
+        this.country = country;
+    }
 
     @Override
     public String toString() {
-        return city + ", " + country;
+        return String.format("%s, %s", city, country);
     }
 }
 
@@ -83,123 +106,87 @@ public class SimpleXMLParser {
         String filePath = "C:/Users/Дарья/IdeaProjects/OOP/src/rundom_structure.xml";
 
         try {
-            // Читаем содержимое файла в строку
-            String xmlData = new String(Files.readAllBytes(Paths.get(filePath)));
-
-            Library library = parseLibrary(xmlData);
-
+            Library library = parseLibrary(filePath);
             System.out.println(library.toString());
-
-        } catch (IOException e) {
+        } catch (Exception e) {
+            System.err.println("Failed to process XML: " + e.getMessage());
             e.printStackTrace();
         }
     }
 
-    // Метод для парсинга всей библиотеки
-    public static Library parseLibrary(String xml) {
+    public static Library parseLibrary(String filePath) throws Exception {
         Library library = new Library();
-        int bookStartIndex = xml.indexOf("<book");
-        while (bookStartIndex != -1) {
-            int bookEndIndex = xml.indexOf("</book>", bookStartIndex);
-            String bookData = xml.substring(bookStartIndex, bookEndIndex + 7);
-            library.books.add(parseBook(bookData));
-            bookStartIndex = xml.indexOf("<book", bookEndIndex);
+        DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+        DocumentBuilder db = dbf.newDocumentBuilder();
+        Document doc = db.parse(Files.newInputStream(Paths.get(filePath)));
+        NodeList bookNodes = doc.getElementsByTagName("book");
+
+        for (int i = 0; i < bookNodes.getLength(); i++) {
+            Element bookElement = (Element) bookNodes.item(i);
+            Book book = parseBook(bookElement);
+            library.addBook(book);
         }
+
         return library;
     }
 
-    // Метод для парсинга одной книги
-    public static Book parseBook(String xml) {
-        Book book = new Book();
+    public static Book parseBook(Element bookElement) {
+        String id = bookElement.getAttribute("id");
+        String title = getTextContent(bookElement, "title");
+        String author = getTextContent(bookElement, "author");
+        int year = Integer.parseInt(getTextContent(bookElement, "year"));
+        String genre = getTextContent(bookElement, "genre");
+        double price = Double.parseDouble(getTextContent(bookElement, "price"));
+        String currency = bookElement.getAttribute("currency");
+        String language = getTextContent(bookElement, "language");
+        String isbn = getTextContent(bookElement, "isbn");
+        String format = getTextContent(bookElement, "format");
 
-        book.id = extractValue(xml, "id=\"", "\"");
-        book.title = extractValue(xml, "<title>", "</title>");
-        book.author = extractValue(xml, "<author>", "</author>");
-        book.year = Integer.parseInt(extractValue(xml, "<year>", "</year>"));
-        book.genre = extractValue(xml, "<genre>", "</genre>");
-        book.price = Double.parseDouble(extractValue(xml, "<price currency=\"", "</price>").split(">")[1]);
-        book.currency = extractValue(xml, "currency=\"", "\"");
+        Publisher publisher = parsePublisher((Element) bookElement.getElementsByTagName("publisher").item(0));
+        List<Review> reviews = parseReviews((Element) bookElement.getElementsByTagName("reviews").item(0));
+        List<String> awards = parseAwards((Element) bookElement.getElementsByTagName("awards").item(0));
 
-        String reviewsData = extractBetween(xml, "<reviews>", "</reviews>");
-        if (!reviewsData.isEmpty()) {
-            int reviewStartIndex = reviewsData.indexOf("<review");
-            while (reviewStartIndex != -1) {
-                int reviewEndIndex = reviewsData.indexOf("</review>", reviewStartIndex);
-                String reviewData = reviewsData.substring(reviewStartIndex, reviewEndIndex + 9);
-                book.reviews.add(parseReview(reviewData));
-                reviewStartIndex = reviewsData.indexOf("<review", reviewEndIndex);
-            }
-        }
-
-        book.language = extractValue(xml, "<language>", "</language>");
-        book.isbn = extractValue(xml, "<isbn>", "</isbn>");
-        book.format = extractValue(xml, "<format>", "</format>");
-
-        String publisherData = extractBetween(xml, "<publisher>", "</publisher>");
-        if (!publisherData.isEmpty()) {
-            book.publisher = parsePublisher(publisherData);
-        }
-
-        String awardsData = extractBetween(xml, "<awards>", "</awards>");
-        if (!awardsData.isEmpty()) {
-            int awardStartIndex = awardsData.indexOf("<award");
-            while (awardStartIndex != -1) {
-                int awardEndIndex = awardsData.indexOf("</award>", awardStartIndex);
-                book.awards.add(extractValue(awardsData, "<award>", "</award>"));
-                awardStartIndex = awardsData.indexOf("<award", awardEndIndex);
-            }
-        }
-
-        return book;
+        return new Book(id, title, author, year, genre, price, currency, language, publisher, isbn, format, reviews, awards);
     }
 
-    // Метод для парсинга отзывов
-    public static Review parseReview(String xml) {
-        Review review = new Review();
-        review.user = extractValue(xml, "<user>", "</user>");
-        review.rating = Integer.parseInt(extractValue(xml, "<rating>", "</rating>"));
-        review.comment = extractValue(xml, "<comment>", "</comment>");
-        return review;
+    public static Publisher parsePublisher(Element publisherElement) {
+        if (publisherElement == null) return null;
+        String name = getTextContent(publisherElement, "name");
+        Element addressElement = (Element) publisherElement.getElementsByTagName("address").item(0);
+        Address address = new Address(getTextContent(addressElement, "city"), getTextContent(addressElement, "country"));
+        return new Publisher(name, address);
     }
 
-    // Метод для парсинга издателя
-    public static Publisher parsePublisher(String xml) {
-        Publisher publisher = new Publisher();
-        publisher.name = extractValue(xml, "<name>", "</name>");
-        String addressData = extractBetween(xml, "<address>", "</address>");
-        if (!addressData.isEmpty()) {
-            publisher.address = parseAddress(addressData);
+    public static List<Review> parseReviews(Element reviewsElement) {
+        List<Review> reviews = new ArrayList<>();
+        if (reviewsElement == null) return reviews;
+        NodeList reviewNodes = reviewsElement.getElementsByTagName("review");
+
+        for (int i = 0; i < reviewNodes.getLength(); i++) {
+            Element reviewElement = (Element) reviewNodes.item(i);
+            String user = getTextContent(reviewElement, "user");
+            int rating = Integer.parseInt(getTextContent(reviewElement, "rating"));
+            String comment = getTextContent(reviewElement, "comment");
+            reviews.add(new Review(user, rating, comment));
         }
-        return publisher;
+
+        return reviews;
     }
 
-    // Метод для парсинга адреса издателя
-    public static Address parseAddress(String xml) {
-        Address address = new Address();
-        address.city = extractValue(xml, "<city>", "</city>");
-        address.country = extractValue(xml, "<country>", "</country>");
-        return address;
+    public static List<String> parseAwards(Element awardsElement) {
+        List<String> awards = new ArrayList<>();
+        if (awardsElement == null) return awards;
+        NodeList awardNodes = awardsElement.getElementsByTagName("award");
+
+        for (int i = 0; i < awardNodes.getLength(); i++) {
+            awards.add(awardNodes.item(i).getTextContent());
+        }
+
+        return awards;
     }
 
-    // Вспомогательные методы для извлечения данных между тегами
-    public static String extractValue(String xml, String startTag, String endTag) {
-        int start = xml.indexOf(startTag) + startTag.length();
-        int end = xml.indexOf(endTag, start);
-        if (start < 0 || end < 0) {
-            return "";
-        }
-        return xml.substring(start, end);
-    }
-
-    public static String extractBetween(String xml, String startTag, String endTag) {
-        int start = xml.indexOf(startTag);
-        if (start == -1) {
-            return "";
-        }
-        int end = xml.indexOf(endTag, start);
-        if (end == -1) {
-            return "";
-        }
-        return xml.substring(start + startTag.length(), end);
+    public static String getTextContent(Element element, String tagName) {
+        NodeList list = element.getElementsByTagName(tagName);
+        return list.getLength() > 0 ? list.item(0).getTextContent() : "";
     }
 }
